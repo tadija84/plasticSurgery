d17545572e401111c354c25bfb701c33cbe6895d
Tadija Selakovic <selakovic.tadija@gmail.com>
2020-06-28 03:23:51 +0200

    submit confirmation added

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 document.getElementById("burger-menu").addEventListener("click", showAside);
 document.getElementById("closeMenu").addEventListener("click", hideAside);
+document.getElementById("form-submit").addEventListener("click", submitForm);
 function showAside(){
     document.getElementById("aside").style.display="block";
     document.getElementById("main").style.width="90vw";
@@ -11,4 +12,10 @@ function hideAside(){
     document.getElementById("main").style.width="100vw";
     document.getElementById("burger-menu").style.display="grid";
     document.getElementById("closeMenu").style.display="none";
+}
+function submitForm(){
+    if(confirm("Submit?")){      
+        document.getElementById("doctors-form").submit();
+        alert("submited");
+    }
 }
